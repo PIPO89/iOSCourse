0675eed36fcbd5793c0cf823c444e0ca87c65ac7
Felipe Soto Campos <fsoto@IMPESA-35.local>
2017-05-22 17:59:25 -0600

    Tarea #2

---
+++ Clase #5/Tarea #2/Tarea #2/CustomCells/NumberTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  NumberTableViewCell.swift
+//  Tarea #2
+//
+//  Created by Felipe Soto Campos on 5/22/17.
+//  Copyright © 2017 PIPO. All rights reserved.
+//
+
+import UIKit
+
+class NumberTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+}
